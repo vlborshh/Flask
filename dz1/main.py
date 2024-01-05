139d074f9d350ab692b55ae4fa8ad59dff221919
vlborshh <vlborshh@yandex.ru>
2024-01-05 13:37:33 +0300

    dz2

--- dz1/main.py
+++ dz1/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login_success.html')
 
 @app.route('/jeans/')
 def jeans():
