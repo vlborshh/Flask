f4e9523a9dc5ae7fef1433107f9a9adf084c46d9
vlborshh <vlborshh@yandex.ru>
2024-01-22 15:41:04 +0300

    dz5

--- dz5/dz.py
+++ dz5/dz.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 from collections import defaultdict
 
 
-dz = FastAPI()
+app_dz = FastAPI()
 
 
 class Task(BaseModel):
@@ -17,17 +17,17 @@ class Task(BaseModel):
 tasks_db: Dict[int, Task] = defaultdict()
 
 
-@app.get("/tasks")
+@app_dz.get("/tasks")
 async def read_tasks(skip: int = 0, limit: int = 100):
     return list(tasks_db.values())[skip: skip + limit]
 
 
-@app.get("/tasks/{task_id}", response_model=Task)
+@app_dz.get("/tasks/{task_id}", response_model=Task)
 async def read_task(task_id: int):
     return tasks_db[task_id]
 
 
-@app.post("/tasks", response_model=Task)
+@app_dz.post("/tasks", response_model=Task)
 async def create_task(task: Task):
     task_id = len(tasks_db) + 1
     db_task = Task(id=task_id, **task.dict())
@@ -35,7 +35,7 @@ async def create_task(task: Task):
     return db_task
 
 
-@app.put("/tasks/{task_id}", response_model=Task)
+@app_dz.put("/tasks/{task_id}", response_model=Task)
 async def update_task(task_id: int, task: Task):
     db_task = tasks_db[task_id]
     for field, value in task.dict(exclude_unset=True).items():
@@ -43,7 +43,7 @@ async def update_task(task_id: int, task: Task):
     return db_task
 
 
-@app.delete("/tasks/{task_id}")
+@app_dz.delete("/tasks/{task_id}")
 async def delete_task(task_id: int):
     del tasks_db[task_id]
     return {"result": "success"}
